1c8979b55850b668854050a70e5b6d187066d955
cailinanne <cailin.nelson@gmail.com>
2013-04-26 15:22:04 -0400

    Hello world.

---
+++ src/main.go
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: cailinnelson
+ * Date: 4/26/13
+ * Time: 3:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world!")
+}
